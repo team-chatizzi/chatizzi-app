29aadf6d062f0c519aee0c7e048003b7e4174d66
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-04-27 02:34:19 -0500

    OAuth with google

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AuthLayout = ({children} : {children: React.ReactNode}) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
         {children}
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,15 @@
-export default function HomePage () {
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+
+export default async function HomePage () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: users } = await supabase.from('users').select()
   return (
     <main>
       <p>Hello Chatizzí</p>
+      {
+        JSON.stringify(users, null, 2)
+      }
     </main>
   )
 }
